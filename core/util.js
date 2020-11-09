b36797e2bcbd70d29afd6c87818a2eecfa06bbda
Pedro Pires <plpires99@gmail.com>
2020-11-09 14:55:01 +0000

    Brighter chat colours

--- core/util.js
+++ core/util.js
@@ -2,19 +2,14 @@ function sanitize(str) {
     return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
 
-let colours = [
-    "red",
-    "blue",
-    "green"
-];
-
-function randomColourOLD(){
-    let index = Math.floor(Math.random() * Math.floor(colours.length));
-    return colours[index];
+// https://jsfiddle.net/1nm8ojxy/
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 function randomColour(){
-    return "#" + Math.random().toString(16).slice(2, 8);
+    //return "#" + Math.random().toString(16).slice(2, 8);
+    return "#" + getRandomArbitrary(0.6, 0.9).toString(16).slice(2, 8);
 }
 
 module.exports = {
